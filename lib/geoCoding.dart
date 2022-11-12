aea3ded7c3d0c59ada196c4654d8d0aef775d043
steffomix <steffomix@gmail.com>
2022-11-12 20:17:45 +0100

    update

---
+++ lib/geoCoding.dart
@@ -0,0 +1,83 @@
+import 'package:http/http.dart' as http;
+import 'package:sprintf/sprintf.dart';
+
+const _url = 'nominatim.openstreetmap.org';
+const _page = '/reverse';
+
+class Address {
+  final DateTime date = DateTime.now();
+  double lat;
+  double lon;
+  String street;
+  String house;
+  String code;
+  String town;
+  Address(this.lat, this.lon, this.street, this.house, this.code, this.town);
+
+  Address.empty()
+      : lat = 0,
+        lon = 0,
+        street = '',
+        house = '',
+        code = '',
+        town = '';
+
+  String address() {
+    return '$street $house, $code $town';
+  }
+}
+
+class Geo {
+  // stores last lookups
+  static final _lookups = <Address>[Address.empty()];
+  static Address _lastLookup = Address.empty();
+
+  // parses Address from openstreetmap response
+  Address _parseAddress(String res, double lat, double lon) {
+    String pattern = r'<%s>(.*)<\/%s>';
+    String streetTag = 'road';
+    String houseTag = 'house_number';
+    String townTag = 'town';
+    String postCodeTag = 'postcode';
+    RegExp street = RegExp(sprintf(pattern, [streetTag, streetTag]));
+    RegExp house = RegExp(sprintf(pattern, [houseTag, houseTag]));
+    RegExp town = RegExp(sprintf(pattern, [townTag, townTag]));
+    RegExp postCode = RegExp(sprintf(pattern, [postCodeTag, postCodeTag]));
+    _lastLookup = Address(
+        lat,
+        lon,
+        street.firstMatch(res)?.group(1) ?? '',
+        house.firstMatch(res)?.group(1) ?? '',
+        postCode.firstMatch(res)?.group(1) ?? '',
+        town.firstMatch(res)?.group(1) ?? '');
+
+    return _lastLookup;
+  }
+
+  ///
+  /// return oldest address
+  Address get address {
+    if (_lookups.isNotEmpty) {
+      Address addr = _lookups.last;
+      _lookups.removeLast();
+      return addr;
+    }
+    return Address.empty();
+  }
+
+  Address get lastAddress {
+    return _lastLookup;
+  }
+
+  // lookup geo location
+  void lookup(double lat, double lon) async {
+    var url =
+        Uri.https(_url, _page, {'lat': lat.toString(), 'lon': lon.toString()});
+
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      Address addr = _parseAddress(response.body, lat, lon);
+      _lookups.add(addr);
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'geoCoding.dart';
 
 void main() => runApp(const App());
 
@@ -9,8 +12,21 @@ class App extends StatefulWidget {
   State<App> createState() => _AppState();
 }
 
+//double _lat = 52.3366473695;
+double _lat = 52.3367;
+double _lon = 9.21645353535;
+
 class _AppState extends State<App> {
+  final geo = Geo();
+  String _addr = '';
+  int _requested = 0;
+
   void _onItemTapped(int index) {
+    geo.lookup(_lat + Random().nextDouble(), _lon + Random().nextDouble());
+    _requested++;
+    var addr = geo.address.address();
+    var lastAddr = geo.lastAddress.address();
+    _addr = '$addr \n $lastAddr';
     setState(() {});
   }
 
@@ -26,14 +42,12 @@ class _AppState extends State<App> {
               ),
               toolbarHeight: 60,
             ),
-            body: Column(children: <Widget>[
-              Row(children: const <Widget>[Text('test'), Text('t2')])
-            ]),
+            body: Center(child: Text('$_requested Address: $_addr ')),
             bottomNavigationBar:
                 BottomNavigationBar(items: const <BottomNavigationBarItem>[
               BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Start'),
               BottomNavigationBarItem(
                   icon: Icon(Icons.list), label: 'Arbeiten'),
-            ], onTap: _onItemTapped, currentIndex: 1)));
+            ], onTap: _onItemTapped, currentIndex: 0)));
   }
 }
