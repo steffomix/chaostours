07ca6aa3dc421fd6c5df4fb203d89912dbd145de
steffomix <steffomix@gmail.com>
2023-01-25 20:14:45 +0100

    -

--- lib/page/widget_tracking_page.dart
+++ lib/page/widget_tracking_page.dart
@@ -14,6 +14,8 @@ import 'package:chaostours/page/widget_add_tasks_page.dart';
 import 'package:chaostours/widget/widget_drawer.dart';
 import 'package:chaostours/widget/widgets.dart';
 import 'package:chaostours/widget/widget_bottom_navbar.dart';
+import 'package:chaostours/address.dart';
+import 'package:chaostours/gps.dart';
 
 class WidgetTrackingPage extends StatefulWidget {
   const WidgetTrackingPage({super.key});
@@ -24,231 +26,106 @@ class WidgetTrackingPage extends StatefulWidget {
 
 class _WidgetTrackingPage extends State<WidgetTrackingPage> {
   static Logger logger = Logger.logger<WidgetTrackingPage>();
+
+  ///
+  /// active trackpoint data
+  static TrackingStatus activeStatus = TrackingStatus.none;
+  static ModelTrackPoint? activeTrackPoint;
+  static List<RunningTrackPoint> activeRunningTrackpoints = [];
+  static String activeNotes = '';
+  static List<ModelTask> activeTasks = [];
+  static List<ModelAlias> activeAlias = [];
+  static Address activeAddress = Address(GPS(0, 0));
+
+  /// recent or saved trackponts
+  static List<ModelTrackPoint> recentTrackpoints = [];
+
   _WidgetTrackingPage() {
-    resetListView();
-    EventManager.listen<EventOnTrackingStatusChanged>(onTrackingStatusChanged);
-    EventManager.listen<EventOnTrackPoint>(onTrackPoint);
+    updateActiveTrackpoint();
+    EventManager.listen<EventOnTick>(onTick);
   }
-  static _ActiveListItem? activeItem;
-  static final List<Widget> listView = [];
+
+  Future<void> updateActiveTrackpoint() async {
+    recentTrackpoints = ModelTrackPoint.recentTrackPoints();
+
+    /// load status
+    activeStatus = TrackingStatus.values.byName(
+        (await Shared(SharedKeys.activeTrackPointStatusName).load()) ??
+            TrackingStatus.none.name);
+
+    /// load active trackpoint
+    String? atp = await Shared(SharedKeys.activeTrackpoint).load();
+    if (atp != null) {
+      activeTrackPoint = ModelTrackPoint.toSharedModel(atp);
+
+      /// load running trackpoints
+      List<String> art =
+          (await Shared(SharedKeys.runningTrackpoints).loadList()) ??
+              <String>[];
+      activeRunningTrackpoints =
+          art.map((e) => RunningTrackPoint.toModel(e)).toList();
+    }
+  }
 
   @override
   void dispose() {
-    EventManager.remove<EventOnTrackingStatusChanged>(onTrackingStatusChanged);
-    EventManager.remove<EventOnTrackPoint>(onTrackPoint);
+    EventManager.remove<EventOnTick>(onTick);
     super.dispose();
   }
 
-  // add a new Trackpoint list item
-  // and prune list to max of 100
-  void onTrackingStatusChanged(EventOnTrackingStatusChanged event) async {
-    activeItem = _ActiveListItem(event.tp);
-    listView.clear();
-    String raw = (await Shared(SharedKeys.recentTrackpoints).load()) ?? '';
-    List<String> recent = raw.trim().split('\n');
-    for (var item in recent) {
-      listView.add(_ActiveListItem(ModelTrackPoint.toSharedModel(item)).widget);
+  void onTick(EventOnTick tick) async {
+    String none = TrackingStatus.none.name;
+    String status =
+        await Shared(SharedKeys.activeTrackPointStatusName).load() ?? none;
+
+    if (status != none) {
+      activeStatus = TrackingStatus.values.byName(status);
+      await updateActiveTrackpoint();
+      await Shared(SharedKeys.activeTrackPointStatusName).save(none);
     }
-    if (mounted) {
-      setState(() {});
-    }
+    setState(() {});
   }
 
-  void onTapItem(TrackPoint trackPoint, TrackingStatus status) {
-    logger.log('OnTapItem');
+  Widget renderActiveTrackpoint(BuildContext context) {
+    if (activeTrackPoint == null) {
+      return (const Center(child: Text('Waiting for active Trackpoint...')));
+    }
+
+    /// try to get an alias from running trackpoints
+    String alias = '- no alias found -';
+    String address = '- no address found yet -';
+    if (activeRunningTrackpoints.isNotEmpty) {
+      List<ModelAlias> aliasList =
+          ModelAlias.nextAlias(activeRunningTrackpoints.last.gps);
+      if (aliasList.isNotEmpty) {
+        alias = aliasList.first.alias;
+      }
+    }
+
+    return Container();
   }
 
-  // update last trackpoint list item
-  void onTrackPoint(EventOnTrackPoint event) async {
-    ModelTrackPoint tp = event.tp;
-    activeItem ??= _ActiveListItem(tp);
-    activeItem?.update(tp);
-    if (mounted) {
-      setState(() {});
-    }
-  }
-
-  void resetListView() {
-    listView.clear();
-    int count = 30;
-    for (var e in ModelTrackPoint.recentTrackPoints(max: count)) {
-      //e.status = TrackingStatus.standing;
-      listView.add(_ActiveListItem(e).widget);
-    }
-    listView
-        .add(const Divider(color: Colors.black, thickness: 3.0, height: 5.0));
-    listView.add(const Center(child: Text('Gespeicherte Einträge')));
-    listView
-        .add(const Divider(color: Colors.black, thickness: 3.0, height: 5.0));
-    listView.add(const Text(''));
-    listView.add(const Text('')); // to be replaced with active item
+  Widget renderRecentTrackPoint(int id) {
+    return Container();
   }
 
   @override
   Widget build(BuildContext context) {
-    resetListView();
-    List<Widget> items = [
-      activeItem?.widget ?? const Text('...waiting for Trackpoint.')
-    ];
-    items.addAll(listView.reversed.toList());
-
-    items.add(TextField(
-      keyboardType: TextInputType.multiline,
-      maxLines: null,
-      controller: TextEditingController(text: ModelTrackPoint.dump()),
-    ));
     return Scaffold(
       appBar: Widgets.appBar(),
       drawer: const WidgetDrawer(),
-      body: ListView(children: items),
+      body: ListView.separated(
+        itemCount: recentTrackpoints.length + 1,
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return renderActiveTrackpoint(context);
+          } else {
+            return renderRecentTrackPoint(index - 1);
+          }
+        },
+      ),
       bottomNavigationBar: const WidgetBottomNavBar(),
-    );
-  }
-}
-
-///
-///
-///
-///
-
-class _ActiveListItem {
-  static Logger logger = Logger.logger<_ActiveListItem>();
-  final ModelTrackPoint event;
-  Widget? _widget;
-  _ActiveListItem(this.event);
-
-  Widget get widget {
-    Widget w = _widget ??= _createWidget();
-    return w;
-  }
-
-  Widget update(ModelTrackPoint tp) {
-    event.deleted = tp.deleted;
-    event.gps = tp.gps;
-    event.address = tp.address;
-    event.idAlias = tp.idAlias;
-    event.timeEnd = DateTime.now();
-    return (_widget = _createWidget());
-  }
-
-  ///
-  /// creates a list item from TrackPoint
-  ///
-  Widget _createWidget() {
-    // calculate duration and distance
-    String duration = event.timeElapsed();
-
-    double distance = event.distance();
-
-    TextStyle fatStyle = const TextStyle(fontWeight: FontWeight.bold);
-
-    // left section (icon)
-    var icon = event.status == TrackingStatus.standing
-        ? Icons.edit
-        : Icons.info_outline;
-
-    Widget left = IconButton(
-      icon: Icon(icon),
-      onPressed: () {
-        EventManager.fire<EventOnMainPaneChanged>(
-            EventOnMainPaneChanged(WidgetAddTasks(trackPoint: event)));
-      },
-    );
-
-    // prepare rows for right section (info)
-    List<TableRow> rows = [];
-    String text;
-
-    ///
-    /// first line (status, time, duration and distance)
-    ///
-    text = event.status == TrackingStatus.moving
-        ? 'Fahren: ${distance}km in $duration'
-        : 'Halt am ${util.formatDate(event.timeStart)}\nfür $duration';
-    rows.add(TableRow(children: <Widget>[
-      TableCell(child: Center(child: Text(text, style: fatStyle)))
-    ]));
-
-    ///// Alias
-    if (event.idAlias.isNotEmpty && event.status == TrackingStatus.standing) {
-      text = ModelAlias.getAlias(event.idAlias.first).alias;
-      rows.add(TableRow(children: <Widget>[
-        TableCell(child: Center(child: Text(text, style: fatStyle)))
-      ]));
-    }
-
-    ///
-    /// second row (address)
-    ///
-    ///// OSM
-    if (event.address.loaded && event.status == TrackingStatus.standing) {
-      text = '(OSM: ${event.address.asString})';
-      rows.add(TableRow(
-          children: <Widget>[TableCell(child: Center(child: Text(text)))]));
-    }
-    ///// Link
-    if (!event.address.loaded &&
-        event.idAlias.isEmpty &&
-        event.status == TrackingStatus.standing) {
-      //https://maps.google.com&q=lat,lon&center=lat,lon
-      text =
-          'GPS: ${(event.gps.lat * 10000).round() / 10000},${(event.gps.lon * 10000).round() / 10000}';
-      rows.add(TableRow(children: <Widget>[Center(child: Text(text))]));
-      //'&center=${event.lat},${event.lon}';
-
-      // rows.add(TableRow(children: <Widget>[
-      //   TableCell(
-      //       child: Center(
-      //           child: InkWell(
-      //     child: Text(gpsText),
-      //     onTap: () {
-      //       launchUrl(
-      //           Uri(scheme: 'https', host: 'maps.google.com', queryParameters: {
-      //         'q': '${event.lat},${event.lon}',
-      //         //'center': '${event.lat},${event.lon}'
-      //       }));
-      //     },
-      //   )))
-      // ]));
-
-    }
-
-    ///
-    /// third row (tasks)
-    ///
-    List<TableRow> tasks = [];
-    ModelTrackPoint model = event;
-    for (var task in model.idTask) {
-      ModelTask.getTask(task);
-      tasks.add(TableRow(children: [
-        const TableCell(child: Text('')),
-        TableCell(
-            child: Center(child: Text('- ${ModelTask.getTask(task).task}')))
-      ]));
-    }
-
-    String gpsText = 'Trackpoints: ${TrackPoint.length}';
-    TableRow gpsInfo = TableRow(children: [
-      const TableCell(child: Text('')),
-      TableCell(child: Text(gpsText))
-    ]);
-
-    // combine right rows to a table
-    Widget right = Table(
-        border: const TableBorder(top: BorderSide(style: BorderStyle.solid)),
-        children: rows);
-
-    // put left section (table with one row) and right section (table with flexible rows) in another table
-    // to simulate html rowspan
-    return Table(
-      columnWidths: const {0: FlexColumnWidth(1), 1: FlexColumnWidth(8)},
-      children: [
-        TableRow(children: [TableCell(child: left), TableCell(child: right)]),
-        const TableRow(
-            children: [TableCell(child: Text('')), TableCell(child: Text(''))]),
-        ...tasks,
-        gpsInfo
-      ],
     );
   }
 }
